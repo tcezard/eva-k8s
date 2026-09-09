228bd5c61f0ebaa5d2c0948fbf93b90e56065299
tcezard <tcezard@ebi.ac.uk>
2026-09-09 14:55:06 +0100

    Remove FTP/HTTP proxy configuration for contig alias and eva-seqcol

--- scripts/maven-settings-to-properties.py
+++ scripts/maven-settings-to-properties.py
@@ -22,8 +22,8 @@ EVA_SEQCOL_MAPPING = {
     'spring.jpa.hibernate.ddl-auto':   '=update',
     'controller.auth.admin.username':  'seqcol.admin-user',
     'controller.auth.admin.password':  'seqcol.admin-password',
-    'ftp.proxy.host':                  '=hh-wwwcache.ebi.ac.uk',
-    'ftp.proxy.port':                  '=3128'
+    'ftp.proxy.host':                  '=null',
+    'ftp.proxy.port':                  '=0'
 }
 
 CONTIG_ALIAS_MAPPING = {
